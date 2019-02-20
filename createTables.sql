af581c4e30fa1ff66b8d6f846fe63268b6396ff0
BartSlenders <45287768+BartSlenders@users.noreply.github.com>
2019-02-20 17:07:18 +0100

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -5,10 +5,10 @@
 
 CREATE UNLOGGED TABLE Degrees(DegreeId int, Dept varchar(50), DegreeDescription varchar(200), TotalECTS int, PRIMARY KEY (DegreeId));
 CREATE UNLOGGED TABLE Students(StudentId int, StudentName varchar(50), Address varchar(200),BirthyearStudent int, Gender char, PRIMARY KEY (StudentId));
-CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationId int, StudentId int, DegreeId int, RegistrationYear int, PRIMARY KEY (StudentRegistrationId));
+CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationId int, StudentId int, DegreeId int, RegistrationYear int, PRIMARY KEY (StudentRegistrationId), FOREIGN KEY StudentId REFERENCES Students(StudentId), FOREIGN KEY DegreeId REFERENCES Degrees(DegreeId));
 CREATE UNLOGGED TABLE Teachers(TeacherId int, TeacherName varchar(50), Address varchar(200), BirthyearTeacher int, Gender char, PRIMARY KEY(TeacherId));
-CREATE UNLOGGED TABLE Courses(CourseId int, CourseName varchar(50), CourseDescription varchar(200),DegreeId int, ETCS int, PRIMARY KEY (CourseId));
-CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int, CourseId int, Year int, Quartile int, PRIMARY KEY (CourseOfferId));
+CREATE UNLOGGED TABLE Courses(CourseId int, CourseName varchar(50), CourseDescription varchar(200),DegreeId int, ETCS int, PRIMARY KEY (CourseId), FOREIGN KEY DegreeId REFERENCES Degrees(DegreeId));
+CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int, CourseId int, Year int, Quartile int, PRIMARY KEY (CourseOfferId), FOREIGN KEY CourseId REFERENCES Courses(CourseId));
 CREATE UNLOGGED TABLE TeacherAssignmentsToCourses(CourseOfferId int, TeacherId int, FOREIGN KEY (CourseOfferId) REFERENCES CourseOffers(CourseOfferId), FOREIGN KEY (TeacherId) REFERENCES Teachers(TeacherId));
 CREATE UNLOGGED TABLE StudentAssistants(CourseOfferId int, StudentRegistrationId int, FOREIGN KEY (CourseOfferId) REFERENCES CourseOffers(CourseOfferId), FOREIGN KEY (StudentRegistrationId) REFERENCES StudentRegistrationsToDegrees(StudentRegistrationId));
 CREATE UNLOGGED TABLE CourseRegistrations(CourseOfferId int, StudentRegistrationId int, Grade varchar(4), FOREIGN KEY (CourseOfferId) REFERENCES CourseOffers(CourseOfferId), FOREIGN KEY (StudentRegistrationId) REFERENCES StudentRegistrationsToDegrees(StudentRegistrationId));
