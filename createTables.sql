513244828a580c6397c0c301add153269912735a
BartSlenders <45287768+BartSlenders@users.noreply.github.com>
2019-02-20 15:36:43 +0100

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -3,4 +3,12 @@
 -- SQL files can be executed from command-line using psql -d THE_DATABASE_NAME -f THE_FILENAME
 -- Notice the UNLOGGED option, which drastically increases performance and reduces space usage
 
-CREATE UNLOGGED TABLE myPhonebook(id int, name varchar(50), address varchar(200), phoneNumber varchar(100));
+CREATE UNLOGGED TABLE Degrees(DegreeId int, Dept varchar(50), DegreeDescription varchar(200), TotalECTS int);
+CREATE UNLOGGED TABLE Students(StudentId int, StudentName varchar(50), Address varchar(200),BirthyearStudent int, Gender char);
+CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationId int, StudentId int, DegreeId int, RegistrationYear int);
+CREATE UNLOGGED TABLE Teachers(TeacherId int, TeacherName varchar(50), Address varchar(200), BirthyearTeacher int, Gender char);
+CREATE UNLOGGED TABLE Courses(CourseId int, CourseName varchar(50), CourseDescription varchar(200),DegreeId int);
+CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int, CourseId int, Year int, Quartile int);
+CREATE UNLOGGED TABLE TeacherAssignmentsToCourses(CourseOfferId int, TeacherId int);
+CREATE UNLOGGED TABLE StudentAssistants(CourseOfferId int, StudentRegistrationId int);
+CREATE UNLOGGED TABLE CourseRegistrations(CourseOfferId int, StudentRegistrationId int, Grade varchar(4);
