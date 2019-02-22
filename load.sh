b4955324346c7fac3f62254e89e907ab3951b834
Wessel Krenn <45484572+WesselKrenn@users.noreply.github.com>
2019-02-22 11:31:52 +0100

    Update load.sh

--- load.sh
+++ load.sh
@@ -4,5 +4,5 @@
 # Expected size of tables after loading should be below 8 GB
 # The total disk space available for storing all data, views, indices, and the write-ahead-log (also see UNLOGGED keyword) is 12 GB
 
-psql -d test2 -f /home/student/Downloads/2ID70-group-08-master/createTables.sql
-psql -d test2 -f /home/student/Downloads/2ID70-group-08-master/loadData.sql
+psql -d uni -f createTables.sql
+psql -d uni -f loadData.sql
