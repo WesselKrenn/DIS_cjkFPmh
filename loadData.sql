938f9849b4002ba617589b932b87a9121d47d1d3
merkelmauer <45287892+merkelmauer@users.noreply.github.com>
2019-02-26 21:18:26 +0100

    Update loadData.sql

--- loadData.sql
+++ loadData.sql
@@ -8,22 +8,22 @@
 -- Don't forget to analyze at the end. It can make a difference in query performance.
 -- Example:
 
-COPY Degrees(DegreeId, Dept, DegreeDescription, TotalECTS) FROM '/home/student/Downloads/tables-500/Degrees.table' DELIMITER ',' CSV HEADER;
-COPY Students(StudentId, StudentName, Address, BirthyearStudent, Gender) FROM '/home/student/Downloads/tables-500/Students.table' DELIMITER ',' CSV HEADER;
-COPY StudentRegistrationsToDegrees(StudentRegistrationId, StudentId, DegreeId, RegistrationYear) FROM '/home/student/Downloads/tables-500/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
-COPY Teachers(TeacherId, TeacherName, Address, BirthyearTeacher, Gender) FROM '/home/student/Downloads/tables-500/Teachers.table' DELIMITER ',' CSV HEADER;
-COPY Courses(CourseId, CourseName, CourseDescription,DegreeId,ETCS) FROM '/home/student/Downloads/tables-500/Courses.table' DELIMITER ',' CSV HEADER;
-COPY CourseOffers(CourseOfferId, CourseId, Year, Quartile) FROM '/home/student/Downloads/tables-500/CourseOffers.table' DELIMITER ',' CSV HEADER;
-COPY TeacherAssignmentsToCourses(CourseOfferId, TeacherId) FROM '/home/student/Downloads/tables-500/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
-COPY StudentAssistants(CourseOfferId, StudentRegistrationId) FROM '/home/student/Downloads/tables-500/StudentAssistants.table' DELIMITER ',' CSV HEADER;
-COPY CourseRegistrations(CourseOfferId, StudentRegistrationId, Grade) FROM '/home/student/Downloads/tables-500/CourseRegistrations.table' DELIMITER ',' CSV HEADER;
+COPY Degrees(DegreeId, Dept, DegreeDescription, TotalECTS) FROM '/home/student/2ID70/tables/Degrees.table' DELIMITER ',' CSV HEADER;
+COPY Students(StudentId, StudentName, Address, BirthyearStudent, Gender) FROM '/home/student/2ID70/tables/Students.table' DELIMITER ',' CSV HEADER;
+COPY StudentRegistrationsToDegrees(StudentRegistrationId, StudentId, DegreeId, RegistrationYear) FROM '/home/student/2ID70/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;
+COPY Teachers(TeacherId, TeacherName, Address, BirthyearTeacher, Gender) FROM '/home/student/2ID70/tables/Teachers.table' DELIMITER ',' CSV HEADER;
+COPY Courses(CourseId, CourseName, CourseDescription,DegreeId,ECTS) FROM '/home/student/2ID70/tables/Courses.table' DELIMITER ',' CSV HEADER;
+COPY CourseOffers(CourseOfferId, CourseId, Year, Quartile) FROM '/home/student/2ID70/tables/CourseOffers.table' DELIMITER ',' CSV HEADER;
+COPY TeacherAssignmentsToCourses(CourseOfferId, TeacherId) FROM '/home/student/2ID70/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;
+COPY StudentAssistants(CourseOfferId, StudentRegistrationId) FROM '/home/student/2ID70/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;
+COPY CourseRegistrations(CourseOfferId, StudentRegistrationId, Grade) FROM '/home/student/2ID70/tables/CourseRegistrations.table' DELIMITER ',' CSV HEADER  NULL 'null';
 
 ALTER TABLE Students add primary key (StudentId);
 ALTER TABLE Degrees add primary key (DegreeId);
 ALTER TABLE Teachers add primary key (TeacherId);
 ALTER TABLE Courses add primary key (CourseId);
 ALTER TABLE CourseOffers add primary key (CourseOfferId);
-ALTER TABLE StudentRegistrationsToDegrees(StudentRegistrationId);
+ALTER TABLE StudentRegistrationsToDegrees add primary key (StudentRegistrationId);
 
 
 
