0dcd24331239324973efc0ace38d239f9ed167e5
BartSlenders <45287768+BartSlenders@users.noreply.github.com>
2019-02-26 15:26:36 +0100

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -3,12 +3,12 @@
 -- SQL files can be executed from command-line using psql -d THE_DATABASE_NAME -f THE_FILENAME
 -- Notice the UNLOGGED option, which drastically increases performance and reduces space usage
 
-CREATE UNLOGGED TABLE Degrees(DegreeId int, Dept varchar(50), DegreeDescription varchar(200), TotalECTS int);
-CREATE UNLOGGED TABLE Students(StudentId int, StudentName varchar(50), Address varchar(200),BirthyearStudent int, Gender char);
-CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationId int, StudentId int, DegreeId int, RegistrationYear int);
-CREATE UNLOGGED TABLE Teachers(TeacherId int, TeacherName varchar(50), Address varchar(200), BirthyearTeacher int, Gender char);
-CREATE UNLOGGED TABLE Courses(CourseId int, CourseName varchar(50), CourseDescription varchar(200),DegreeId int, ETCS int);
-CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int, CourseId int, Year int, Quartile int);
+CREATE UNLOGGED TABLE Degrees(DegreeId int, Dept varchar(50), DegreeDescription varchar(200), TotalECTS smallint);
+CREATE UNLOGGED TABLE Students(StudentId int, StudentName varchar(50), Address varchar(200),BirthyearStudent smallint, Gender char);
+CREATE UNLOGGED TABLE StudentRegistrationsToDegrees(StudentRegistrationId int, StudentId int, DegreeId int, RegistrationYear smallint);
+CREATE UNLOGGED TABLE Teachers(TeacherId int, TeacherName varchar(50), Address varchar(200), BirthyearTeacher smallint, Gender char);
+CREATE UNLOGGED TABLE Courses(CourseId int, CourseName varchar(50), CourseDescription varchar(200),DegreeId int, ETCS smallint);
+CREATE UNLOGGED TABLE CourseOffers(CourseOfferId int, CourseId int, Year smallint, Quartile smallint);
 CREATE UNLOGGED TABLE TeacherAssignmentsToCourses(CourseOfferId int, TeacherId int);
 CREATE UNLOGGED TABLE StudentAssistants(CourseOfferId int, StudentRegistrationId int);
-CREATE UNLOGGED TABLE CourseRegistrations(CourseOfferId int, StudentRegistrationId int, Grade varchar(4));
+CREATE UNLOGGED TABLE CourseRegistrations(CourseOfferId int, StudentRegistrationId int, Grade smallint NULL);
