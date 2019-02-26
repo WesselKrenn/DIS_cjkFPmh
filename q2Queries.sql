14ae544e0fb16873f2e18c0cc41d034fd5df96f9
merkelmauer <45287892+merkelmauer@users.noreply.github.com>
2019-02-26 23:10:45 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -5,7 +5,7 @@
 -- Example: Let's say I only know sub-question 3:
 with PassedCourses(CourseName, Grade, Year, Quartile, CourseOfferId) as (SELECT CourseName, Grade, Year, Quartile, co.CourseOfferId FROM Courses c, CourseOffers co, CourseRegistrations cr WHERE c.CourseId = co.CourseId and co.CourseOfferId = cr.CourseOfferID and grade >= 5 and DegreeId = %2% and StudentRegistrationId in (SELECT StudentRegistrationId FROM StudentRegistrationsToDegrees WHERE StudentId = %1%) ORDER BY year, quartile, CourseOfferId) SELECT CourseName, grade FROM PassedCourses;
 SELECT 0;
-SELECT phonenumber from myphonebook where name=%1%; 
+with GenPerDept(amount, gen) as (SELECT count(Gender), Gender FROM Students s, StudentRegistrationsToDegrees srd, degrees d WHERE Dept = %1% and d.DegreeId = srd.DegreeId and s.StudentId = srd.StudentId GROUP BY Gender) SELECT 100*amount(SELECT sum(amount) FROM GenPerDept) FROM GenPerDept WHERE Gen = 'F'; 
 SELECT 0;
 SELECT 0;
 SELECT 0;
