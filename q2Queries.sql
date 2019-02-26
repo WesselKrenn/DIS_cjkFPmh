48874800735c7dcc05dfa7d0dab8b0ecf379c492
merkelmauer <45287892+merkelmauer@users.noreply.github.com>
2019-02-26 23:07:17 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -3,7 +3,7 @@
 -- Do not break long queries to more than one line, and do not leave empty lines between them. 
 -- If you do not know the answer to one of the sub-questions, then write SELECT 0;
 -- Example: Let's say I only know sub-question 3:
-SELECT 0;
+with PassedCourses(CourseName, Grade, Year, Quartile, CourseOfferId) as (SELECT CourseName, Grade, Year, Quartile, co.CourseOfferId FROM Courses c, CourseOffers co, CourseRegistrations cr WHERE c.CourseId = co.CourseId and co.CourseOfferId = cr.CourseOfferID and grade >= 5 and DegreeId = %2% and StudentRegistrationId in (SELECT StudentRegistrationId FROM StudentRegistrationsToDegrees WHERE StudentId = %1%) ORDER BY year, quartile, CourseOfferId) SELECT CourseName, grade FROM PassedCourses;
 SELECT 0;
 SELECT phonenumber from myphonebook where name=%1%; 
 SELECT 0;
