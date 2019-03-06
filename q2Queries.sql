51b99d93a6686f5eeb27328a8d6569ce4c7ca12d
BartSlenders <45287768+BartSlenders@users.noreply.github.com>
2019-03-06 16:56:20 +0100

    updates from WesselKrenn1 Branch

--- q2Queries.sql
+++ q2Queries.sql
@@ -11,3 +11,68 @@ with AmountPassed(amount, courseid) as (SELECT count(Grade), c.CourseId FROM Cou
 SELECT 0;
 SELECT 0;
 SELECT 0;
+
+
+
+
+-- From wesselKrenn1:
+
+\timing
+--with PassedCourses(CourseName, Grade, Year, Quartile, CourseOfferId) as (SELECT CourseName, Grade, Year, Quartile, co.CourseOfferId FROM Courses c, CourseOffers co, CourseRegistrations cr WHERE c.CourseId = co.CourseId and co.CourseOfferId = cr.CourseOfferID and grade >= 5 and DegreeId = 1 and StudentRegistrationId in (SELECT StudentRegistrationId FROM StudentRegistrationsToDegrees WHERE StudentId = 1) ORDER BY year, quartile, CourseOfferId) SELECT CourseName, grade FROM PassedCourses;
+--with PassedCourses(CourseName, Grade,year, QUartile, CourseOfferId) as (Select CourseName, Grade, Year, Quartile, cco.CourseOfferId from courses_courseoffers cco, courseregistrations cr where cco.courseofferid = cr.courseofferid and grade >= 5 and degreeid = 1 and studentregistrationid in (select studentregistrationid from studentregistrationstodegrees where studentid = 1) order by year, quartile, courseofferid) select coursename, grade from passedcourses;
+
+
+-- Q1 
+-- takes 26s
+SELECT Courses.CourseName, CourseRegistrations.Grade FROM Students INNER JOIN StudentRegistrationsToDegrees on (Students.StudentId = StudentRegistrationsToDegrees.StudentId) INNER JOIN CourseRegistrations on (StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId) INNER JOIN CourseOffers on (CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId) INNER JOIN Courses on (CourseOffers.CourseId = Courses.CourseId) WHERE (Students.StudentId = 2 AND StudentRegistrationsToDegrees.DegreeId = 1099);
+-- takes 12s
+with CurrentStudent as (SELECT * FROM StudentRegistrationsToDegrees WHERE degreeid = 1099 AND studentid = 2) SELECT c.CourseName, cr.Grade FROM Students s INNER JOIN CurrentStudent cs on (s.studentid = cs.studentid) INNER JOIN CourseRegistrations cr on (cs.studentregistrationid = cr.studentregistrationid) INNER JOIN CourseOffers co on (cr.courseofferid = co.courseofferid) INNER JOIN Courses c on (co.courseid = c.courseid);
+-- takes 2s but no result
+with CurrentStudent as (SELECT * FROM StudentRegistrationsToDegrees WHERE degreeid = 1099 AND studentid = 2), CurrentCourses as (Select courseid, coursename, degreeid FROM courses where degreeid = 1099) SELECT cc.coursename, cr.grade FROM Currentstudent cs INNER JOIN CourseRegistrations cr on (cr.studentregistrationid = cs.studentregistrationid) INNER JOIN courseoffers co on (cr.courseofferid = co.courseofferid) INNER JOIN currentcourses cc on (co.courseid = cc.courseid);
+-- takes 14s
+with currentStudent as (SELECT * FROM StudentRegistrationsToDegrees WHERE degreeid = 1099 AND studentid = 2) SELECT c.CourseName, cr.Grade FROM currentstudent cs INNER JOIN CourseRegistrations cr on (cs.studentregistrationid = cr.studentregistrationid) INNER JOIN CourseOffers co on (cr.courseofferid = co.courseofferid) INNER JOIN Courses c on (co.courseid = c.courseid);
+-- Takes 0.2s lol (primary key swapped from coid, sr to sr,coid) + added hash index on srtd(sid)
+SELECT c.CourseName, creg.Grade FROM StudentRegistrationsToDegrees as sreg, CourseRegistrations as creg, CourseOffers as coffer, Courses as c where sreg.StudentId = %1% and sreg.StudentRegistrationId = creg.StudentRegistrationId AND creg.CourseOfferId = coffer.CourseOfferId AND coffer.CourseId = c.CourseId AND sreg.DegreeId = %2%;
+-- Q2 
+-- takes a bit more than 1 minute (is executed 10 times so 10 mins aka not good)
+SELECT DISTINCT StudentRegistrationsToDegrees.StudentId FROM StudentRegistrationsToDegrees INNER JOIN GPAAndECTSCount on (StudentRegistrationsToDegrees.StudentRegistrationId = GPAAndECTSCount.StudentRegistrationId) INNER JOIN CourseRegistrations on (GPAAndECTSCount.StudentRegistrationId = CourseRegistrations.StudentRegistrationId) INNER JOIN Degrees on (StudentRegistrationsToDegrees.DegreeId = Degrees.DegreeId) WHERE (GPAAndECTSCount.TotalECTSAcquired >= Degrees.TotalECTS AND CourseRegistrations.Grade > 5.0 AND GPAAndECTSCount.GPA > 9.0);
+-- Duurt ages > 10 minutes aka SHIT
+WITH crf (regId) AS (SELECT DISTINCT cr.StudentRegistrationId FROM CourseRegistrations AS cr WHERE cr.Grade < 5), temp (StudId, DegId) AS (SELECT DISTINCT srtd.StudentId, srtd.DegreeId FROM (SELECT * FROM CourseRegistrations AS cr WHERE NOT EXISTS (SELECT * FROM crf WHERE crf.regId = cr.StudentRegistrationId)) as cre INNER JOIN CourseOffers AS co ON cre.courseOfferId = co.courseOfferId INNER JOIN Courses AS c ON c.CourseId = co.CourseId INNER JOIN StudentRegistrationsToDegrees AS srtd ON srtd.StudentRegistrationId = cre.StudentRegistrationId INNER JOIN Degrees AS d ON d.DegreeId = srtd.DegreeId GROUP BY srtd.StudentId, srtd.DegreeId, d.TotalECTS HAVING AVG(cre.Grade) = 9.5 AND SUM(c.ECTS) >= d.TotalECTS) SELECT DISTINCT temp.StudId FROM temp;
+-- Q3 
+-- Takes 18 seconds good enough I guess
+SELECT Degrees.DegreeId, (cast(SUM(CASE WHEN Students.Gender = 'F' THEN 1 ELSE 0 END) as float) / COUNT(Students.Gender)) as female_percent
+FROM Students INNER JOIN StudentRegistrationsToDegrees on (Students.StudentId = StudentRegistrationsToDegrees.StudentId) INNER JOIN Degrees on (StudentRegistrationsToDegrees.DegreeId = Degrees.DegreeId)
+GROUP BY Degrees.DegreeId;
+-- Q4
+-- takes a few (1-6) seconds (is executed 10 times)
+SELECT (cast(SUM(CASE WHEN Students.Gender = 'F' THEN 1 ELSE 0 END) as float) / COUNT(Students.Gender)) as female_percent
+FROM Students INNER JOIN StudentRegistrationsToDegrees on (Students.StudentId = StudentRegistrationsToDegrees.StudentId) INNER JOIN Degrees on (StudentRegistrationsToDegrees.DegreeId = Degrees.DegreeId)
+WHERE (Degrees.Dept = %1%);
+-- Q5
+-- Takes almost 2 minutes (executed 5 times so still too slow)
+SELECT CourseId, COUNT(Grade), COUNT(CASE WHEN Grade >= 5 THEN 1 END), CAST(COUNT(CASE WHEN Grade >= 5 THEN 1 END) AS FLOAT) / COUNT(Grade) AS percentagePassing FROM CourseRegistrations INNER JOIN CourseOffers ON (CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId) WHERE Grade IS NOT NULL GROUP BY CourseId;
+-- Q6
+-- takes 33 seconds (executed 3 times (1, 2, 3)
+SELECT * FROM (SELECT StudentRegistrationId, COUNT(CASE WHEN CourseRegistrations.Grade = MaxGrades.MaxGrade THEN 1 END) AS NrOfExcellentCourses FROM CourseRegistrations INNER JOIN MaxGrades ON (CourseRegistrations.CourseOfferId = MaxGrades.CourseOfferId) GROUP BY StudentRegistrationId) AS s WHERE nrOfExcellentCourses >= %1%;
+-- Q7 
+-- check for actieve studenten moet nog toegevoegd worden
+SELECT StudentRegistrationsToDegrees.DegreeId, Students.BirthyearStudent, Students.Gender, AVG(CourseRegistrations.Grade) FROM StudentRegistrationsToDegrees INNER JOIN Students on (StudentRegistrationsToDegrees.StudentId = Students.StudentId) INNER JOIN CourseRegistrations on (StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId) GROUP BY (StudentRegistrationsToDegrees.DegreeId, Students.BirthyearStudent, Students.Gender);
+-- Q8
+-- !!!!!!! Doesnt run !!!!!!!
+SELECT Courses.CourseName, CourseOffers.Year, CourseOffers.Quartile
+FROM (SELECT Courses.CourseId, Courses.CourseName, CourseOffers.Year, CourseOffers.Quartile FROM Courses INNER JOIN CourseOffers on (Courses.CourseId = CourseOffers.CourseId) INNER JOIN CourseRegistrations on (CourseOffers.CourseOfferId = CourseRegistrations.CourseOfferId) INNER JOIN StudentAssistants on (CourseOffers.CourseOfferId = StudentAssistants.CourseOfferId)
+GROUP BY Courses.CourseId, CourseOffers.Year, CourseOffers.Quartile
+HAVING COUNT(CourseRegistrations.CourseOfferId) / 50 < COUNT(StudentAssistants.CourseOfferId)) as s;
+-- Runs but kinda slow
+SELECT CourseName, Year, Quartile
+FROM (SELECT Courses.CourseId, Courses.CourseName, CourseOffers.Year, CourseOffers.Quartile FROM Courses INNER JOIN CourseOffers on (Courses.CourseId = CourseOffers.CourseId) INNER JOIN CourseRegistrations on (CourseOffers.CourseOfferId = CourseRegistrations.CourseOfferId) INNER JOIN StudentAssistants on (CourseOffers.CourseOfferId = StudentAssistants.CourseOfferId)
+GROUP BY Courses.CourseId, CourseOffers.Year, CourseOffers.Quartile
+HAVING COUNT(CourseRegistrations.CourseOfferId) /50 +1 > COUNT(StudentAssistants.CourseOfferId)) as s;
+-- marko met zn reee
+with reee(courseofferid, cnt) as (SELECT courseofferid, count(studentregistrationid) FROM courseregistrations GROUP BY courseofferid), reee2(courseofferid, cnt2) as (SELECT courseofferid, count(studentregistrationid) FROM studentassistants GROUP BY courseofferid) SELECT(reee.courseofferid FROM reee r INNER JOIN reee2 r2 ON(r.courseofferid = r2.courseofferid) WHERE cnt / 50 + 1 > cnt);
+-- QUERY 6
+--SELECT DISTINCT StudentId, count(StudentId) FROM HighestGradeCourseOffers WHERE Year = 2018 AND Quartile = 1 GROUP BY StudentId HAVING count(studentid) >= 1;
+-- QUERY 7
+--SELECT degreeid, birthyearstudent, gender, avg(gpa) FROM Students LEFT JOIN ActiveStudentsPerDegree ON ActiveStudentsPerDegree.StudentId = Students.StudentId LEFT JOIN GPA ON GPA.StudentRegistrationId = ActiveStudentsPerDegree.StudentRegistrationId GROUP BY cube(degreeid, birthyearstudent, gender);
+-- QUERY 8
+--SELECT CourseName, Year, Quartile FROM NumAssistantsCourseOffer LEFT JOIN NumStudentsCourseOffer ON NumAssistantsCourseOffer.CourseOfferId = NumStudentsCourseOffer.CourseOfferId AND NumStudents/NumAssistants>50 LEFT JOIN CourseOffers ON NumStudentsCourseOffer.CourseOfferId = CourseOffers.CourseOfferId LEFT JOIN Courses ON Courses.CourseId = CourseOffers.CourseOfferId ORDER BY CourseOffers.CourseOfferId;
