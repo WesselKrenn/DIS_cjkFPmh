b60a68b7ada3e1afb036380cf71bdc89f6c76b40
Wessel Krenn <45484572+WesselKrenn@users.noreply.github.com>
2019-03-08 16:36:54 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,7 +1,7 @@
 with CurrentStudent(StudentRegistrationId) as (SELECT StudentRegistrationId FROM StudentRegistrationsToDegrees WHERE degreeid = %2% and studentid = %1%) SELECT CourseName, grade FROM CourseRegistrations cr INNER JOIN CurrentStudent srd ON (cr.StudentRegistrationId = srd.StudentRegistrationId) INNER JOIN CourseOffers co ON (co.courseofferid = cr.courseofferid) WHERE grade >=5 ORDER BY year, quartile, co.courseofferid;
 SELECT 0;
-SELECT 0;
 SELECT count(CASE WHEN S.Gender = 'F'  THEN 1 END)/count(SR.StudentId)::float as percentageFemale FROM Degrees D, StudentRegistrationsToDegrees SR, Students S WHERE SR.DegreeId = D.DegreeId and S.StudentId = SR.StudentId and D.Dept = %1%;
+SELECT 0;
 SELECT 0;
 SELECT 0;
 SELECT 0;
