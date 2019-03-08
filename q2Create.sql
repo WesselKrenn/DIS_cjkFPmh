62a573c86f1f5cbfb342df761dc6d062ac523acb
merkelmauer <45287892+merkelmauer@users.noreply.github.com>
2019-03-08 22:08:41 +0100

    Update q2Create.sql

--- q2Create.sql
+++ q2Create.sql
@@ -1,1 +1,2 @@
 CREATE INDEX idx_SRTD ON StudentRegistrationsToDegrees USING hash(StudentId);
+CREATE MATERIALIZED VIEW MaxGradesQ1 AS SELECT co.CourseOfferId, MAX(Grade) AS MaxGrade FROM CourseRegistrations cr INNER JOIN CourseOffers co ON (cr.courseofferid = co.courseofferid) WHERE Year = 2018 AND Quartile = 1 GROUP BY co.CourseOfferId;
