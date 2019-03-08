74c9594ef905c03238493656e2c7346beabaeb9c
merkelmauer <45287892+merkelmauer@users.noreply.github.com>
2019-03-08 22:45:59 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -5,4 +5,8 @@ SELECT (cast(SUM(CASE WHEN s.Gender = 'F' THEN 1 ELSE 0 END) as float) / COUNT(s
 SELECT CourseId, COUNT(CASE WHEN Grade >= 5 THEN 1 END)*100/ (cast(Count(Grade) as float)) AS percentagePassing FROM CourseRegistrations WHERE Grade IS NOT NULL GROUP BY CourseId;
 SELECT * FROM(SELECT StudentId, COUNT(CASE WHEN cr.Grade = mg.MaxGrade THEN 1 END) AS numberOfCoursesWhereExcellent FROM CourseRegistrations cr INNER JOIN MaxGradesQ1 mg ON (cr.CourseOfferId = mg.CourseOfferId) GROUP BY StudentId) as subquery WHERE numberOfCoursesWhereExcellent >= %1%;
 SELECT 0;
-SELECT 0;
+with reee(courseofferid, cnt) as (SELECT courseofferid, count(studentregistrationid) FROM courseregistrations GROUP BY courseofferid), 
+reee2(courseofferid, cnt2) as (SELECT courseofferid, count(studentregistrationid) FROM studentassistants GROUP BY courseofferid),
+reee3(courseofferid, cnt, year, quartile, coursename) as (SELECT r.courseofferid, cnt, year, quartile, coursename FROM reee r INNER JOIN courseoffers co ON (co.courseofferid = r.courseofferid))  
+
+SELECT coursename, year, quartile FROM reee3 r INNER JOIN reee2 r2 ON(r.courseofferid = r2.courseofferid) WHERE cnt2 < (((cnt - 1) / 50) + 1);
