136560a9b0986cc008e839110c415aa12c9dffb8
Wessel Krenn <45484572+WesselKrenn@users.noreply.github.com>
2019-03-09 16:24:24 +0100

    Update q2Queries.sql
    added q2

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,5 +1,5 @@
 with CurrentStudent(StudentRegistrationId) as (SELECT StudentRegistrationId FROM StudentRegistrationsToDegrees WHERE degreeid = %2% and studentid = %1%) SELECT CourseName, grade FROM CourseRegistrations cr INNER JOIN CurrentStudent srd ON (cr.StudentRegistrationId = srd.StudentRegistrationId) INNER JOIN CourseOffers co ON (co.courseofferid = cr.courseofferid) WHERE grade >=5 ORDER BY year, quartile, co.courseofferid;
-SELECT 0;
+SELECT distinct pps.StudentId FROM pointsPerStudent pps, studentNoFails SNF WHERE pps.StudentRegistrationId = SNF.studentRegistrationId and pps.GPA > %1% ORDER BY pps.studentid;
 SELECT 0;
 SELECT (cast(SUM(CASE WHEN s.Gender = 'F' THEN 1 ELSE 0 END) as float) / COUNT(s.Gender)) as percentage FROM Students s INNER JOIN StudentRegistrationsToDegrees srd on (s.StudentId = srd.StudentId) INNER JOIN Degrees d on (srd.DegreeId = d.DegreeId) WHERE (d.Dept =  %1%);
 SELECT CourseId, COUNT(CASE WHEN Grade >= 5 THEN 1 END)*100/ (cast(Count(Grade) as float)) AS percentagePassing FROM CourseRegistrations WHERE Grade IS NOT NULL GROUP BY CourseId;
